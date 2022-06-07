273960ad8d626b04235b85fff50ac68340c57158
beargame123 <hoyoung7827@gmail.com>
2022-06-07 13:18:00 +0900

    .

---
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/TodayIsDiarysApplication.java
@@ -0,0 +1,13 @@
+package com.example.today_is_diarys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayIsDiarysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayIsDiarysApplication.class, args);
+    }
+
+}
---
+++ Today_is_diarys/src/test/java/com/example/today_is_diarys/TodayIsDiarysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.today_is_diarys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayIsDiarysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
