40f42ff1aa9984e977f8e1808a32c5879f94a21b
beargame123 <hoyoung7827@gmail.com>
2022-06-09 23:44:38 +0900

    ➕UserRepository 추가

---
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.today_is_diarys.user.repository;
+
+import com.example.today_is_diarys.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
