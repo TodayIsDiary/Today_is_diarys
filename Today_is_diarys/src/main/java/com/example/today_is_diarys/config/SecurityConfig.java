de9bcab5ba0112ad8b9e60f053997c4527e1f1f6
beargame123 <hoyoung7827@gmail.com>
2022-06-13 00:06:40 +0900

    ➕security config 추가

---
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.today_is_diarys.config;
+
+import com.example.today_is_diarys.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+
+    @Override
+    public void configure(WebSecurity web){
+        web
+                .ignoring().antMatchers("/js/**","/h2-console/**","/img/**","/css/**","resource/**", "jsp/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/login","/signup","/user").permitAll()
+                    .antMatchers("/").hasRole("USER")
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/main")
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
