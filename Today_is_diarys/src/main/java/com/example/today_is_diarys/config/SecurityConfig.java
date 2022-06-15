25df4edae0c03a4b71861120e39c2e064ab66607
beargame123 <hoyoung7827@gmail.com>
2022-06-15 23:16:44 +0900

    🛠️passwordEncoder 추가

--- Today_is_diarys/src/main/java/com/example/today_is_diarys/config/SecurityConfig.java
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.example.today_is_diarys.config;
 
-import com.example.today_is_diarys.user.service.UserService;
+import com.example.today_is_diarys.filter.JwtAuthenticationFilter;
+import com.example.today_is_diarys.token.utile.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void configure(WebSecurity web){
@@ -22,25 +27,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .ignoring().antMatchers("/js/**","/h2-console/**","/img/**","/css/**","resource/**", "jsp/**");
     }
 
+    //암호화에 필요한 PasswordEncoder를 Bean으로 등록합니다.
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic().and()
+                .httpBasic().disable()
                 .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                     .antMatchers("/login","/signup","/ad/**").permitAll()
                     .antMatchers("/").hasRole("USER")
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(new JwtAuthenticationFilter((jwtTokenProvider)),
+                            UsernamePasswordAuthenticationFilter.class)
                     .logout()
                         .logoutSuccessUrl("/login")
                         .invalidateHttpSession(true);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService)
-                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
