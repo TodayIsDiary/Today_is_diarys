91a5cc9fb2b98717b82d177da1d56902b10ebf71
beargame123 <hoyoung7827@gmail.com>
2022-06-15 23:20:25 +0900

    🛠️signup,login 코드 수정

--- Today_is_diarys/src/main/java/com/example/today_is_diarys/user/controller/UserController.java
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.today_is_diarys.user.controller;
 
+import com.example.today_is_diarys.token.utile.JwtTokenProvider;
 import com.example.today_is_diarys.user.dto.UserInfoDto;
-import com.example.today_is_diarys.user.service.UserService;
+import com.example.today_is_diarys.user.entity.User;
+import com.example.today_is_diarys.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,17 +16,30 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody UserInfoDto dto){
-        userService.save(dto);
-        return "hi";
+    public void signup(@RequestBody UserInfoDto dto){
+                 userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .nickName(dto.getNickName())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .age(dto.getAge())
+                .sex(dto.getSex())
+                .role("ROLE_USER").build()).getId();
     }
 
     @PostMapping("/ad/signup")
-    public void adsignup(@RequestBody UserInfoDto dto){
-        userService.adsave(dto);
+    public void adsignup(){
+        userRepository.save(User.builder()
+                .email("admin@gmail.com")
+                .nickName("bearKing")
+                .password(passwordEncoder.encode("bear"))
+                .age(99L)
+                .sex(1L)
+                .role("ROLE_ADMIN,ROLE_USER").build()).getId();
     }
 
     @GetMapping(value = "/logout")
@@ -33,5 +48,19 @@ public class UserController {
         return "redirect/logout";
     }
 
+    @PostMapping("/login")
+    public String login(@RequestBody UserInfoDto dto){
+        User user = userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(()-> new IllegalArgumentException("가입하지 않은 Email입니다 (ㅡ_ㅡ)"));
+        if(!passwordEncoder.matches(dto.getPassword(), user.getPassword())){
+            throw new IllegalStateException("잘못된 비밀번호 입니다.");
+        }
+        return jwtTokenProvider.createToken(user.getEmail(), user.getRole());
+    }
 
+    @DeleteMapping("/leave/{id}")
+    public String leave(@PathVariable Long id){
+        userRepository.deleteById(id);
+        return "회원정보가 삭제되었습니다...";
+    }
 }
