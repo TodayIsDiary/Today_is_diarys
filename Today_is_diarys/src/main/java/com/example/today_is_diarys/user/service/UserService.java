15d41bdfcfbade4d82d482c62743036e50ca213e
beargame123 <hoyoung7827@gmail.com>
2022-06-15 23:17:51 +0900

    🛠️Spring bean순환 참조땜에 code controller로 옮김

--- Today_is_diarys/src/main/java/com/example/today_is_diarys/user/service/UserService.java
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/user/service/UserService.java
@@ -1,12 +1,11 @@
 package com.example.today_is_diarys.user.service;
 
-import com.example.today_is_diarys.user.dto.UserInfoDto;
 import com.example.today_is_diarys.user.entity.User;
 import com.example.today_is_diarys.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,29 +19,4 @@ public class UserService implements UserDetailsService {
         return userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException(email));
     }
 
-    public Long save(UserInfoDto dto){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        dto.setPassword(encoder.encode(dto.getPassword()));
-
-        return userRepository.save(User.builder()
-                .email(dto.getEmail())
-                .nickName(dto.getNickName())
-                .password(dto.getPassword())
-                .age(dto.getAge())
-                .sex(dto.getSex())
-                .role("ROLE_USER").build()).getId();
-    }
-
-    public Long adsave(UserInfoDto dto){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        dto.setPassword(encoder.encode(dto.getPassword()));
-
-        return userRepository.save(User.builder()
-                .email("admin@gmail.com")
-                .nickName("bearKing")
-                .password("bear")
-                .age(99L)
-                .sex(1L)
-                .role("ROLE_ADMIN,ROLE_USER").build()).getId();
-    }
 }
