2f65420288b22b13e36d90697c5518973b4446ca
beargame123 <hoyoung7827@gmail.com>
2022-07-06 00:06:28 +0900

    ➕::CORS 방지코드 추가

---
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.example.today_is_diarys.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
