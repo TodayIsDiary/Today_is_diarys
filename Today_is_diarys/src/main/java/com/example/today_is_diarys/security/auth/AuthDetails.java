10ea1d757447016102560e0bf44f3e466446881d
beargame123 <hoyoung7827@gmail.com>
2022-07-06 20:19:50 +0900

    ➕:: secuirtyDetails구현

---
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/security/auth/AuthDetails.java
@@ -0,0 +1,56 @@
+package com.example.today_is_diarys.security.auth;
+
+import com.example.today_is_diarys.entity.user.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getUsername(){
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
