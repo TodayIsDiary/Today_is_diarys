1d18b1425c6f5756f4588ffd1a45054427e6dfea
beargame123 <hoyoung7827@gmail.com>
2022-07-06 20:20:27 +0900

    ➕:: securityDetailsService 개발

---
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/security/auth/AuthDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.today_is_diarys.security.auth;
+
+import com.example.today_is_diarys.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+}
