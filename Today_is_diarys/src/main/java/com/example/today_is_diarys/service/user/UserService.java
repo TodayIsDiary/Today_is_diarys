0d35c9559215aa42860f46ff8a3cb0b1831a5964
beargame123 <hoyoung7827@gmail.com>
2022-07-13 14:05:58 +0900

    🛠️::Mypage에 대한 로직 수정

--- Today_is_diarys/src/main/java/com/example/today_is_diarys/service/user/UserService.java
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/service/user/UserService.java
@@ -1,12 +1,13 @@
 package com.example.today_is_diarys.service.user;
 
-import com.example.today_is_diarys.dto.user.request.UserInfoDto;
-import com.example.today_is_diarys.dto.user.response.UserDto;
+import com.example.today_is_diarys.dto.user.response.UserMy;
+import com.example.today_is_diarys.dto.user.response.UserSet;
 import com.example.today_is_diarys.entity.user.User;
 import com.example.today_is_diarys.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
@@ -14,30 +15,26 @@ public class UserService{
 
     private final UserRepository userRepository;
 
-    public void SetNk(UserInfoDto dto, Long id){
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found"));
-        user.setNK(dto.getNickName());
-        userRepository.save(user);
-    }
-
-    public void SetIc(UserInfoDto dto, Long id){
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found"));
-        user.setIC(dto.getIntroduce());
-        userRepository.save(user);
-    }
-
-    public UserDto getUser(String email){
+    public UserMy getUser(String email){
         User user = userRepository.findByEmail(email).orElseThrow(()-> new BadCredentialsException("회원을 찾을수없습니다."));
-        return UserDto.builder()
-                .id(user.getId())
-                .nickName(user.getNickName())
-                .email(user.getEmail())
-                .password(user.getPassword())
-                .sex(user.getSex())
+        return UserMy.builder()
                 .age(user.getAge())
-                .introduce(user.getIntroduce())
-                .role(user.getRole())
+                .nickName(user.getNickName())
                 .build();
+    }
+
+    public UserSet setUser(String email){
+        User user = userRepository.findByEmail(email).orElseThrow(()-> new BadCredentialsException("회원을 찾을수없습니다."));
+        return UserSet.builder()
+                .introduce(user.getIntroduce())
+                .nickName(user.getNickName())
+                .build();
+    }
+
+    public void setUsers(String email, UserSet userSet){
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new BadCredentialsException("회원을 찾을수없습니다."));
+        user.setUser(userSet.getNickName(), userSet.getIntroduce());
+        userRepository.save(user);
     }
 
 }
