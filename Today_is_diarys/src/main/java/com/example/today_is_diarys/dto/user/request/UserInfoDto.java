a26c65df83202dfd8ca8e2dc39b8566f5d3a6d06
beargame123 <hoyoung7827@gmail.com>
2022-07-13 14:04:39 +0900

    ➕::id 추가

--- Today_is_diarys/src/main/java/com/example/today_is_diarys/dto/user/request/UserInfoDto.java
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/dto/user/request/UserInfoDto.java
@@ -1,13 +1,17 @@
 package com.example.today_is_diarys.dto.user.request;
 
 import com.example.today_is_diarys.security.auth.enums.Role;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserInfoDto {
+    private Long id;
     private String email;
     private String password;
     private Long sex;
@@ -15,4 +19,16 @@ public class UserInfoDto {
     private Long age;
     private String nickName;
     private String introduce;
+
+    @Builder
+    public UserInfoDto(Long id, String email, String password, Long sex, Role role, Long age, String nickName, String introduce){
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.sex = sex;
+        this.role = role;
+        this.age = age;
+        this.nickName = nickName;
+        this.introduce = introduce;
+    }
 }
