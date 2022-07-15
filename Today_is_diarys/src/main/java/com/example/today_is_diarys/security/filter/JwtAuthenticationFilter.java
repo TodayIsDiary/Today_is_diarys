8cbd520a1812fed99ca9527d0f09d4bc40edd3ec
beargame123 <hoyoung7827@gmail.com>
2022-07-15 09:37:53 +0900

    🛠️:: filter 로직 수정

--- Today_is_diarys/src/main/java/com/example/today_is_diarys/security/filter/JwtAuthenticationFilter.java
+++ Today_is_diarys/src/main/java/com/example/today_is_diarys/security/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.today_is_diarys.security.filter;
 
 import com.example.today_is_diarys.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -14,14 +15,20 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends GenericFilterBean {
     private final JwtTokenProvider jwtTokenProvider;
 
+    // request 로 들어가는 Jwt 의 유효성을 검증 - JwtProvider.validationToken() 을 필터로서 FilterChain 에 추가
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        //헤더에서 filter를 받아온다.
+        // request 에서 token을 취함.
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
-        //유효한 토큰인지 확인한다.
+
+        //검증
+        log.info("토큰을 확인중입니다...");
+        log.info(((HttpServletRequest) request).getRequestURI());
+
         if(token != null && jwtTokenProvider.validateToken(token)){
             //토큰이 유효하면 토큰으로부터 유저 정보를 가져옵니다.
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
